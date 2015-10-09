c9a100c07c16f6cc9acfd931f783e7ab3ec26166
DEsipov <for29you@gmail.com>
2015-10-09 22:42:29 +0300

    Create mini proxy and add test data file.

--- habraproxy.py
+++ habraproxy.py
@@ -1,9 +1,37 @@
 #!-*-coding:utf-8-*-
+
 __author__ = 'virgo'
 
+import SocketServer
+import SimpleHTTPServer
+import urllib
+
+PORT = 8000
+DOMAIN = 'http://habrahabr.ru'
+FNAME = 'habra.html'
+DEBUG = True
+
+class Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # self.copyfile(urllib.urlopen(self.path), self.wfile)
+        try:
+            if not DEBUG:
+                html = urllib.urlopen(DOMAIN + self.path)
+                f = open(FNAME, 'w')
+                f.write(html.read())
+                f.close()
+            f = open(FNAME, 'r')
+            self.copyfile(f, self.wfile)
+        except Exception, ex:
+            self.close_connection()
 
 def main():
     print 'Coming soon...'
+    httpd = SocketServer.ForkingTCPServer(('', PORT), Proxy)
+    print "serving at port", PORT
+    if not DEBUG:
+        httpd.serve_forever()
+    httpd.handle_request()
 
 
 if __name__ == '__main__':
